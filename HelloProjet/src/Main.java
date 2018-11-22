8b3ba91c9b3cdf9c0ecf197545be84083144b820
liubostudyjava <liubo6688>
2018-11-22 21:27:34 +0800

    改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld1!");
+        System.out.println("HelloWorld1!");
 
         System.out.println("bs—Ó¿•!");
         System.out.println("HelloWorld!");
