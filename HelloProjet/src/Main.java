552cf05291e111807f2154ae6df7e8ab5711f20c
liubostudyjava <liubo6688>
2018-11-22 21:36:49 +0800

    去

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,14 +5,6 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("bs—Ó¿•!");
-        System.out.println("HelloWorld!");
-        System.out.println("sb—Ó¿•!");
-        System.out.println("sb—Ó¿•1!");
-        System.out.println("sb—Ó¿•3!");
-        System.out.println("sb—Ó¿•2!");
-        System.out.println("sb—Ó¿•45!");
-        System.out.println("sb—Ó¿•5!");
-        System.out.println("sb—Ó¿•6!");
+       
     }
 }
